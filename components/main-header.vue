85652271cf3969e161bc5e63e4e74c1a8c68a1bb
Afej <afekurojoshua@gmail.com>
2022-03-16 22:12:13 +0100

    feat: demo mobile nav (testing)

--- components/main-header.vue
+++ components/main-header.vue
@@ -1,11 +1,46 @@
 <template>
-  <header class="flex items-center justify-between wrapper">
-    <div class="flex items-center justify-between">
-      <nuxt-link to="/" class="logo mr-48 cursor-pointer">
-        <img src="@/assets/images/shared/desktop/logo.svg" alt="audiophile" />
-      </nuxt-link>
-      
-      <ul class="nav flex items-center justify-between text-white">
+  <header
+    class="
+      flex
+      md:items-center
+      items-start
+      justify-between
+      wrapper
+      md:py-8
+      py-6
+    "
+  >
+    <div class="md:flex md:items-center justify-between">
+      <div class="flex justify-between items-center gap-11">
+        <div class="md:hidden icon text-2xl text-white duration-500">
+          <i
+            :class="[open ? 'fa-solid fa-xmark' : 'fa-solid fa-bars']"
+            @click="toggleNav"
+          ></i>
+        </div>
+        <nuxt-link to="/" class="logo mr-48 cursor-pointer">
+          <img src="@/assets/images/shared/desktop/logo.svg" alt="audiophile" />
+        </nuxt-link>
+      </div>
+
+      <ul
+        class="
+          nav
+          md:flex md:items-center
+          justify-between
+          text-white
+          md:py-0
+          py-10
+          md:w-auto
+          w-full
+          md:static
+          absolute
+          top-14
+          duration-700
+          ease-in
+        "
+        :class="[open ? 'left-12' : '-left-full']"
+      >
         <li>
           <nuxt-link to="/"> Home </nuxt-link>
         </li>
@@ -27,16 +62,26 @@
   </header>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
 
-export default Vue.extend({})
+<script>
+export default {
+  data() {
+    return {
+      open: true,
+    }
+  },
+  methods: {
+    toggleNav() {
+      this.open = !this.open
+    },
+  },
+}
 </script>
+
 
 <style lang="scss" scoped>
 header {
   border-bottom: 1px solid rgba(255, 255, 255, 0.2);
-  padding: 32px 0;
 }
 
 .logo {
